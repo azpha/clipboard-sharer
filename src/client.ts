6f542ed9c427e5abfa7f8357ae5b71c799ccca29
Alex Frantz <alex@alexav.gg>
2025-11-08 16:17:16 -0500

    use other clipboard

--- src/client.ts
+++ src/client.ts
@@ -5,20 +5,23 @@ import "dotenv/config";
 
 const socket = io(process.env.SOCKET_IP || "http://localhost:3000");
 let lastContent = "";
+let fromOtherMachine = "";
 
 socket.on("connect", () => {
   console.log("socket connected to server");
   watchClipboard();
 });
 socket.on("copy", (args) => {
+  fromOtherMachine = args;
   notifier.notify({
     title: "Clipboard Sharer",
     message: "A new string is available for copy!",
+    wait: true,
   });
 });
 
 notifier.on("click", async function () {
-  await clipboardy.write(lastContent);
+  await clipboardy.write(fromOtherMachine);
 });
 
 function watchClipboard() {
